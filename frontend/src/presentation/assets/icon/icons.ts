1cb6abd57a2aff1d7b416b883aa203302085c8f6
Victor <victorh.pedr@gmail.com>
2024-06-25 15:18:38 -0300

    fix: error deploy

--- frontend/src/presentation/assets/icon/icons.ts
+++ frontend/src/presentation/assets/icon/icons.ts
@@ -2,8 +2,9 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 import { IoPersonOutline } from "react-icons/io5";
 import { FaArrowRightLong } from "react-icons/fa6";
 import { AiOutlineClose } from "react-icons/ai";
+import { IoIosArrowDown } from "react-icons/io";
 
-const Icons = {
+const Icons = {IoIosArrowDown,
   FaArrowRightLong,
   IoPersonOutline,
   AiOutlineClose,
