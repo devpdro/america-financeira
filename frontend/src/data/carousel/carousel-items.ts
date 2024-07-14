1941e51a670748a3c543a68fba116f53440271c8
Victor <victorh.pedr@gmail.com>
2024-07-14 20:04:50 -0300

    fix: error deploy

--- frontend/src/data/carousel/carousel-items.ts
+++ frontend/src/data/carousel/carousel-items.ts
@@ -5,24 +5,28 @@ const CarouselItems: CarouselItemsTypes[] = [
     key: 1,
     title: "Empréstimo Pessoal",
     subtitle: "Dinheiro rápido pra usar como quiser e condições exclusivas.",
+    link: "/emprestimo-consignado",
   },
   {
     key: 2,
     title: "Empréstimo Consignado",
     subtitle:
       "Para aposentados e pensionistas do INSS com taxas a partir de 1,90% a.m.",
+    link: "/emprestimo-consignado",
   },
   {
     key: 3,
     title: "Empréstimo FGTS",
     subtitle:
       "Antecipe até 10 parcelas do seu saque-aniversário FGTS com a menor taxa do mercado",
+    link: "/emprestimo-consignado",
   },
   {
     key: 4,
     title: "Consórcio",
     subtitle:
       "Adquira imóveis, veículos ou serviços com planejamento e sem juros altos.",
+    link: "/emprestimo-consignado",
   },
 ];
 
