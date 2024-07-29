612f220f2e5fe395b982b76e94a8ae28c4e8da54
Victor <victorh.pedr@gmail.com>
2024-07-29 01:45:40 -0300

    fix: error deploy

--- src/presentation/components/form/index.ts
+++ src/presentation/components/form/index.ts
@@ -2,3 +2,4 @@ export { default as FgtsAdvanceRequest } from "./fgts-request/fgts-request";
 export { default as LoginRequest } from "./login-request/login-request";
 export { default as LoanRequest } from "./loan-request/loan-request";
 export { default as Button } from "./button/button";
+export { default as Input } from "./input/input";
--- src/presentation/components/form/login-request/login-request.tsx
+++ src/presentation/components/form/login-request/login-request.tsx
@@ -1,13 +1,9 @@
-import { Button } from "@/presentation/components/form";
-
 import styles from "./login-request.module.scss";
-import Image from "next/image";
-import { Images } from "@/presentation/assets";
 
 const LoginRequest = () => {
   return (
     <form className={styles.container}>
-      <Image src={Images.AboutIlustration} />
+      <h1>Teste</h1>
     </form>
   );
 };
