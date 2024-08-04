61889afe7ef6010a6593c7309da3f909822b099c
Victor <victorh.pedr@gmail.com>
2024-08-04 19:13:06 -0300

    fix: error deploy

--- src/infra/storage/cookie-manager.tsx
+++ src/infra/storage/cookie-manager.tsx
@@ -9,14 +9,7 @@ type CookieProps = {
 }
 
 export const setCookie = (name: string, value: string, options?: CookieProps) => {
-  const cookieOptions = {
-    ...options,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict' as 'Strict' | 'Lax' | 'None',
-  }
-
-  Cookie.set(name, value, cookieOptions)
-  console.log('Cookie set for:', name)
+  Cookie.set(name, value, { ...options, secure: true, sameSite: 'strict' })
 }
 
 export const getCookie = (name: string): string | undefined => {
@@ -26,7 +19,3 @@ export const getCookie = (name: string): string | undefined => {
 export const removeCookie = (name: string) => {
   Cookie.remove(name)
 }
-
-// Esta linha deve ser usada apenas se você está importando funções de outro arquivo
-// e deseja re-exportá-las a partir deste.
-// export * from './cookie-manager';
--- src/main/providers/cookies-providers.tsx
+++ src/main/providers/cookies-providers.tsx
@@ -1,24 +1,14 @@
 import { createContext, useContext, ReactNode } from 'react'
-import { setCookie, getCookie, removeCookie } from '@/infra/storage'
-
-interface CookieFunctions {
-  setCookie: (name: string, value: string, options?: any) => void
-  getCookie: (name: string) => string | undefined
-  removeCookie: (name: string) => void
-}
+import * as cookieManager from '@/infra/storage/cookie-manager'
 
 interface CookieProviderProps {
   children: ReactNode
 }
 
-export const CookieContext = createContext<CookieFunctions>({
-  setCookie: () => {},
-  getCookie: () => undefined,
-  removeCookie: () => {},
-})
+export const CookieContext = createContext<any>(null)
 
 export const useCookies = () => useContext(CookieContext)
 
 export const CookieProvider = ({ children }: CookieProviderProps) => (
-  <CookieContext.Provider value={{ setCookie, getCookie, removeCookie }}>{children}</CookieContext.Provider>
+  <CookieContext.Provider value={cookieManager}>{children}</CookieContext.Provider>
 )
