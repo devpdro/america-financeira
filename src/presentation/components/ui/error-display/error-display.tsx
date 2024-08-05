077f8510b8d778ae71681d4dec87ae14fab74f48
Victor <victorh.pedr@gmail.com>
2024-08-05 02:38:05 -0300

    fix: finished styles responsive error

--- src/presentation/components/ui/error-display/error-display.tsx
+++ src/presentation/components/ui/error-display/error-display.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { Button } from '@/presentation/components/form'
 
 import styles from './error-display.module.scss'
@@ -7,12 +9,16 @@ const ErrorDisplay = () => {
     <section className={styles.container}>
       <h1 className={styles.title}>Ops!</h1>
       <p className={styles.subtitle}>
-        Não conseguimos encontrar a página <br /> que você está procurando.
+        Não conseguimos encontrar a página que você <br />  está procurando.
       </p>
       <p className={styles.subtitle}>
         Se não sabe para onde ir, qualquer caminho serve! <br /> - Gato Cheshire, Alice no País das Maravilhas.
       </p>
-      <Button width="300px" typeStyle="btn2" text="Voltar para a página principal" />
+      <Link
+        href="/"
+      >
+        <Button width="230px" typeStyle="btn2" text="Voltar para a página principal" />
+      </Link>
     </section>
   )
 }
