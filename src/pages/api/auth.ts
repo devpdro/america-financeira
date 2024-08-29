1e3dbb653788a6a2db84f5eb2c6218f6dbe70187
Victor <victorh.pedr@gmail.com>
2024-08-29 08:35:54 -0300

    feat: adding user collaborats

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { serialize } from 'cookie'
 import jwt from 'jsonwebtoken'
-import db from '@/lib/db.mjs' // Substitua pelo caminho correto para o db.ts
+import db from '@/lib/db.mjs' 
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY || 'your-secret-key'
 
@@ -9,17 +9,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { email, password } = req.body
 
-    // Procura o usuário no banco de dados Nedb
+   
     db.findOne({ email, password }, (err, user) => {
       if (err) {
         return res.status(500).json({ message: 'Erro interno no servidor' })
       }
 
       if (user) {
-        // Gera um token JWT
+
         const token = jwt.sign({ id: user._id, email: user.email }, SECRET_KEY, { expiresIn: '1d' })
 
-        // Define o cookie com o token de autenticação
+
         res.setHeader(
           'Set-Cookie',
           serialize('auth-token', token, {
