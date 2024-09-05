8834a4145c3bbe1d3752913f36f7e49122f21d06
Victor <victorh.pedr@gmail.com>
2024-09-05 15:21:56 -0300

    feat: adding users test

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { serialize } from 'cookie'
 import jwt from 'jsonwebtoken'
-import db from '@/lib/db.mjs'
+
+import { users } from '@/data/ui'
 
 const SECRET_KEY = process.env.JWT_SECRET_KEY || 'your-secret-key'
 
@@ -9,31 +10,27 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { email, password } = req.body
 
-    db.findOne({ email, password }, (err, user) => {
-      if (err) {
-        return res.status(500).json({ message: 'Erro interno no servidor' })
-      }
+    const user = users.find((user) => user.email === email && user.password === password)
 
-      if (user) {
-        const token = jwt.sign({ id: user._id, email: user.email }, SECRET_KEY, { expiresIn: '1d' })
+    if (user) {
+      const token = jwt.sign({ email: user.email }, SECRET_KEY, { expiresIn: '1d' })
 
-        res.setHeader(
-          'Set-Cookie',
-          serialize('auth-token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
-            maxAge: 60 * 60 * 24,
-            path: '/',
-          })
-        )
+      res.setHeader(
+        'Set-Cookie',
+        serialize('auth-token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: 'strict',
+          maxAge: 60 * 60 * 24,
+          path: '/',
+        })
+      )
 
-        return res.status(200).json({ message: 'Login realizado com sucesso' })
-      }
+      return res.status(200).json({ message: 'Login realizado com sucesso' })
+    }
 
-      return res.status(401).json({ message: 'E-mail ou senha inválidos' })
-    })
-  } else {
-    return res.status(405).json({ message: 'Método não permitido' })
+    return res.status(401).json({ message: 'E-mail ou senha inválidos' })
   }
+
+  return res.status(405).json({ message: 'Método não permitido' })
 }
