7c3f3fd1a71bb3e1094118180c61d2462a3b20d4
Victor <victorh.pedr@gmail.com>
2024-12-18 12:37:36 -0300

    feat: adding

--- front-end/src/presentation/components/ui/team/team.tsx
+++ front-end/src/presentation/components/ui/team/team.tsx
@@ -7,12 +7,12 @@ import classNames from 'classnames'
 import S from './team.module.scss'
 
 const TEAMPHOTOS = [
-  { src: IMAGE.CONFRATERNIZACAO_TROFEU, alt: 'Equipe unida posando para foto' },
-  { src: IMAGE.QUEM_SOMOS, alt: 'Foto final da equipe em evento de confraternização' },
-  { src: IMAGE.PARCEIROS, alt: 'Equipe assistindo apresentação na TV' },
+  { src: IMAGE.EQUIPE_TODOS_AMARELO, alt: 'Equipe unida posando para foto' },
+  { src: IMAGE.EQUIPE_TODA, alt: 'Foto final da equipe em evento de confraternização' },
+  { src: IMAGE.DUAS_MENINAS, alt: 'Equipe assistindo apresentação na TV' },
   { src: IMAGE.EQUIPE_TODOS_VERMELHO, alt: 'Allan durante a confraternização da equipe' },
-  { src: IMAGE.FOUNDER_CONFRATERNIZACAO, alt: 'Equipe toda vestida de vermelho para evento especial' },
-  { src: IMAGE.EQUIPE_UNIDA, alt: 'Equipe celebrando com troféu em mãos' },
+  { src: IMAGE.CONFRATERNIZACAO_TROFEU, alt: 'Equipe toda vestida de vermelho para evento especial' },
+  { src: IMAGE.PARCEIROS, alt: 'Equipe celebrando com troféu em mãos' },
 ]
 
 type TeamProps = {
@@ -32,9 +32,10 @@ const Team = ({ width, height }: TeamProps) => (
       <div className={classNames(S.col, S['col-3'])}>
         <div className={S['img-square']}>
           <Image
-            src={TEAMPHOTOS[3].src}
-            alt={TEAMPHOTOS[3].alt}
+            src={TEAMPHOTOS[5].src}
+            alt={TEAMPHOTOS[5].alt}
             className={S['img-cover']}
+            layout="responsive"
             height={height}
             width={width}
           />
@@ -43,9 +44,16 @@ const Team = ({ width, height }: TeamProps) => (
       <div className={classNames(S.col, S['col-6'])}>
         <div className={classNames(S.row, S['form-row'], S['align-items-end'], S.mb2)}>
           {TEAMPHOTOS.slice(1, 3).map((img, index) => (
-            <div key={index} className={classNames(S.col, S[`col-${index === 0 ? 7 : 5}`])}>
+            <div key={index} className={classNames(S.col, S[`col-${index === 0 ? 7 : 6}`])}>
               <div className={S['img-square']}>
-                <Image width={width} height={height} src={img.src} alt={img.alt} className={S['img-cover']} />
+                <Image
+                  layout="responsive"
+                  width={width}
+                  height={height}
+                  src={img.src}
+                  alt={img.alt}
+                  className={S['img-cover']}
+                />
               </div>
             </div>
           ))}
@@ -54,7 +62,14 @@ const Team = ({ width, height }: TeamProps) => (
           {TEAMPHOTOS.slice(3, 5).map((img, index) => (
             <div key={index} className={classNames(S.col, S[`col-${index === 0 ? 5 : 7}`])}>
               <div className={S['img-square']}>
-                <Image width={width} height={height} src={img.src} alt={img.alt} className={S['img-cover']} />
+                <Image
+                  width={width}
+                  height={height}
+                  layout="responsive"
+                  src={img.src}
+                  alt={img.alt}
+                  className={S['img-cover']}
+                />
               </div>
             </div>
           ))}
@@ -63,8 +78,8 @@ const Team = ({ width, height }: TeamProps) => (
       <div className={classNames(S.col, S['col-3'])}>
         <div className={S['img-square']}>
           <Image
-            src={TEAMPHOTOS[1].src}
-            alt={TEAMPHOTOS[1].alt}
+            src={TEAMPHOTOS[0].src}
+            alt={TEAMPHOTOS[0].alt}
             className={S['img-cover']}
             layout="responsive"
             height={height}
